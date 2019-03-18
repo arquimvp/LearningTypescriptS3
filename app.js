efc4dc0b48f6f9e7acc33a6a79652ebfc65c9964
arquimvp <alberto.martinez@kairosds.com>
2019-03-17 22:08:18 -0600

    master - feature(app): Propiedades y metodos estaticos - app.ts

--- app.js
+++ app.js
@@ -1,81 +1,27 @@
 "use strict";
 /*
- Herencia y metodos protegidos.
+ Metodos y propiedades estáticos:
+
+  Las propiedades y metodos estaticos son aquellos que podemos llamar sin necesidad de instanciar la clase.
+
  */
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-//Abreviacion de propiedades en clases: Mucho codigo => menos codigo
-//1. Mucho codigo:
-var Desarrollador = /** @class */ (function () {
-    function Desarrollador(nombre, perfil) {
-        this.nombre = nombre;
-        this.perfil = perfil;
-    }
-    return Desarrollador;
-}());
-//2. Menos codigo:
 var Developer = /** @class */ (function () {
-    //De esta manera se pondran automaticamente las propiedades de name y profile a cualquier clase o variable del tipo Developer
-    function Developer(name, profile) {
-        // 9. Descomentar despues de usar el super en la clase Frontend:
-        //console.log('constructor de Developer invocado en segundo termino');
-        this.name = name;
-        this.profile = profile;
+    function Developer() {
+        this.vidaSocial = true;
     }
+    Developer.crearDeveloper = function () {
+        return new Developer();
+    };
+    //Las propiedades estaticas no son accesibles en una instancia. Solo son accesibles haciendo referencia a la clase directamente.
+    //son como una especia de contantes.
+    Developer.perfil = 'fullstack';
     return Developer;
 }());
-var ironDev = new Developer('IronDev', 'Ionic');
-console.log(ironDev);
-/*****************************************************************************************************************************
- * HERENCIA: Darle las propiedades y metodos a las clases hijas.
- */
-// 3. Aqui la clase Frontend extiende (hereda) de Developer:
-var Frontend = /** @class */ (function (_super) {
-    __extends(Frontend, _super);
-    function Frontend() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return Frontend;
-}(Developer));
-//4. Creo una nueva instancia de Frontend (ironFront):
-//  en este caso es importante observar que estoy utilizando el cosntructor de Developer, dado que Frontend no tiene constructor.
-//  Si le quito los parametros a Frontend, mandaria error:
-//5. Error:
-//  let ironFront : Frontend = new Frontend();
-//6. Correcto: (aunque toma el constructor de la clase padre o superclase, dado que Frontend no cuenta con su propio constructor):
-var ironFront = new Frontend("Ironfront", 'flutter');
-console.log(ironFront);
-//7. Una vez que has codificado hasta aqui, es hora de implementar el llamdo super en el constructor de la clase "Frontend":
-//Ve y descomenta el codigo del constructor en clase Frontend
-/*
-  10 - Crearemos un metodo en la clase Developer para poder heredarlo a la clase Frontend:
-  ve al codigo de la clase Developer.
-  Una vez descomentado el metodo de getName, yo puedo ejecutar el metodo a traves de ironFront!!!
-*/
-//console.log(ironFront.getName());
-/**
- * 12. PROTECTED!!! Cambiar el metodo getName a protected en la clase Developer.
- * esto con la finalidad de poder acceder a un metodo protected desde una subclase que es una de las 2 unicas formas
- * de accesar a ese tipo de metodos protegidos, llamandolos desde la misma clase o subclases.
- *
- */
-/**
- * 13. Ahora en mi clase Frontend voy  generar una metodo publico ('getNombre') que va a llamar al metodo protected (getName)
- * de la clase Developer.
- * Esto lo puedo realizar agregando el super() en el metodo getNombre de la clase Frontend:
- *
- *
- *
- *
- */ 
+//Aqui estoy llamando a la propiedad perfil de la clase sin necsidad de instanciar.
+console.log(Developer.perfil);
+// Aqui puedo instanciar apartir de un metodo estatico:
+var devFS = Developer.crearDeveloper();
+console.log(devFS);
+var dev09 = new Developer();
+console.log(dev09);
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,115 +1,37 @@
 /*
- Herencia y metodos protegidos.
- */
+ Metodos y propiedades estáticos:
 
-//Abreviacion de propiedades en clases: Mucho codigo => menos codigo
+  Las propiedades y metodos estaticos son aquellos que podemos llamar sin necesidad de instanciar la clase.
 
-
-//1. Mucho codigo:
-class Desarrollador {
-
-  public nombre: string;
-  public perfil: string;
-
-  constructor(nombre: string, perfil: string) {
-    this.nombre = nombre;
-    this.perfil = perfil;
-  }
-
-}
-
-//2. Menos codigo:
-
-class Developer {
-
-  //De esta manera se pondran automaticamente las propiedades de name y profile a cualquier clase o variable del tipo Developer
-  constructor(public name : string, private profile : string) { 
-    // 9. Descomentar despues de usar el super en la clase Frontend:
-    //console.log('constructor de Developer invocado en segundo termino');
-
-  }
-
-
-  // 11. Descomentar metodo y utilizarlo con ironFront:
-
-  // public getName(): string{
-  //   return this.name;
-  // }
-
-  // 15. poner en el metodo de getName:
-  // console.log('getName Developer protegido');
-
-
-}
-
-
-let ironDev : Developer = new Developer('IronDev', 'Ionic');
-
-console.log(ironDev);
-
-
-/***************************************************************************************************************************** 
- * HERENCIA: Darle las propiedades y metodos a las clases hijas.
  */
 
 
- // 3. Aqui la clase Frontend extiende (hereda) de Developer:
-class Frontend extends Developer {
+ class Developer {
 
-
-// 8. Los constructores de clases heredadas deben contener un llamado super.
-  // constructor(a:string, b:string){
-  //   console.log('ahora si, esta invocando en primer lugar al constructor de la clase "Frontend"');
-  //   super(a,b);  //Esta es la linea super que permite inicializar el constructor padre e invocar tambien metodos del padre.
-  // }
-
-
+  //Las propiedades estaticas no son accesibles en una instancia. Solo son accesibles haciendo referencia a la clase directamente.
+  //son como una especia de contantes.
+  static perfil : string = 'fullstack';
   
-  // 14. public getNombre():string{
-    //console.log('getNombre Frontend (publico)');
-  //   return super.getName();
-  // }
+  vidaSocial:boolean = true;
 
-}
+  constructor(){
+    
+  }
 
-
-//4. Creo una nueva instancia de Frontend (ironFront):
-//  en este caso es importante observar que estoy utilizando el cosntructor de Developer, dado que Frontend no tiene constructor.
-//  Si le quito los parametros a Frontend, mandaria error:
-
-//5. Error:
-//  let ironFront : Frontend = new Frontend();
-
-//6. Correcto: (aunque toma el constructor de la clase padre o superclase, dado que Frontend no cuenta con su propio constructor):
-let ironFront : Frontend = new Frontend("Ironfront", 'flutter');
-
-console.log(ironFront);
-
-//7. Una vez que has codificado hasta aqui, es hora de implementar el llamdo super en el constructor de la clase "Frontend":
-//Ve y descomenta el codigo del constructor en clase Frontend
+  static crearDeveloper(){
+    return new Developer();
+  }
+ }
 
 
-/* 
-  10 - Crearemos un metodo en la clase Developer para poder heredarlo a la clase Frontend:
-  ve al codigo de la clase Developer.
-  Una vez descomentado el metodo de getName, yo puedo ejecutar el metodo a traves de ironFront!!!
-*/
+//Aqui estoy llamando a la propiedad perfil de la clase sin necsidad de instanciar.
+ console.log(Developer.perfil);
+ 
+// Aqui puedo instanciar apartir de un metodo estatico:
+ let devFS = Developer.crearDeveloper();
 
-//console.log(ironFront.getName());
+ console.log(devFS);
 
-/** 
- * 12. PROTECTED!!! Cambiar el metodo getName a protected en la clase Developer.
- * esto con la finalidad de poder acceder a un metodo protected desde una subclase que es una de las 2 unicas formas
- * de accesar a ese tipo de metodos protegidos, llamandolos desde la misma clase o subclases.
- * 
- */
+ let dev09 = new Developer();
 
- /** 
-  * 13. Ahora en mi clase Frontend voy  generar una metodo publico ('getNombre') que va a llamar al metodo protected (getName)
-  * de la clase Developer.
-  * Esto lo puedo realizar agregando el super() en el metodo getNombre de la clase Frontend:
-  * 
-  * 
-  * 
-  * 
-  */
+ console.log(dev09);
